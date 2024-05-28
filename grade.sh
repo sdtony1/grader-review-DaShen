c3a1c1a4599d8c120cba699716bd8ec182b87cb4
沈达 <shenda@Tonys-MacBook-Pro.local>
2024-05-28 16:01:29 -0700

    Some concise message describing the change

--- grade.sh
+++ grade.sh
@@ -11,6 +11,24 @@ echo 'Finished cloning'
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
+# Step 2
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    echo "ListExamples.java file found."
+else 
+    echo "ListExamples.java file not found."
+    echo "Grade: 0"
+    exit
+fi
 
+# Step 3
+cp TestListExamples.java student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+
+# Step 4
+cd grading-area
+javac -cp $CPATH *.java
+
+echo "The exit code for the compile step is $?." 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
